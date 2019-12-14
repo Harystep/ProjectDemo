fc8fc00b44c6d5200b59e5c0a515204c4d278f2c
huangran_iOS <warm588@163.com>
2019-12-14 17:45:54 +0800

    Initial Commit

---
+++ HCProjectFrameDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HCProjectFrameDemo
+//
+//  Created by pxsl on 2019/12/14.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ HCProjectFrameDemoTests/HCProjectFrameDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  HCProjectFrameDemoTests.m
+//  HCProjectFrameDemoTests
+//
+//  Created by pxsl on 2019/12/14.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HCProjectFrameDemoTests : XCTestCase
+
+@end
+
+@implementation HCProjectFrameDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
